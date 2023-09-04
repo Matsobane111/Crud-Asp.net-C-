4cab7b7c112589246c39a7f608b539a644a1c884
Maropeng Makgato <msinah4u@gmail.com>
2023-09-04 03:15:35 +0200

    adding the details view

---
+++ CrudAssessment/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using CrudAssessment.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAssessment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        string connString = "Data Source=DESKTOP-QTH4LPK\\SQLEXPRESS;Initial Catalog=store2;Integrated Security=True;";
+        public ActionResult Index()
+        {
+            List<BookModelView> Books = new List<BookModelView>();
+            string query = "SELECT * FROM Books";
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            Books.Add(new BookModelView
+                            {
+                                book_id = Convert.ToInt32(sdr["book_id"]),
+                                title = Convert.ToString(sdr["title"]),
+                                author = Convert.ToString(sdr["author"]),
+                                price = Convert.ToString(sdr["price"]),
+                                quantity_available = Convert.ToInt32(sdr["quantity_available"])
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            if (Books.Count == 0)
+            {
+                Books.Add(new BookModelView());
+            }
+            return View(Books);
+        }
+
+
+        public ActionResult BookDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookModelView Book = new BookModelView();
+            string query = "SELECT * FROM Books where book_id=" + id;
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            Book = new BookModelView
+                            {
+                                book_id = Convert.ToInt32(sdr["book_id"]),
+                                title = Convert.ToString(sdr["title"]),
+                                author = Convert.ToString(sdr["author"]),
+                                price = Convert.ToString(sdr["price"]),
+                                quantity_available = Convert.ToInt32(sdr["quantity_available"])
+                            };
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            if (Book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Book);
+        }
+    }
+}
---
+++ CrudAssessment/Models/BookModelView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrudAssessment.Models
+{
+    public class BookModelView
+    {
+        [Display(Name = "Book ID")]
+        public int book_id { get; set; }
+
+        [Required(ErrorMessage = "Title is required."), Display(Name = "Title")]
+        public string title { get; set; }
+
+        [Required(ErrorMessage = "Author is required."), Display(Name = "Author")]
+        public string author { get; set; }
+
+
+        [Required(ErrorMessage = "Expenses is required."), Display(Name = "Price")]
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valid Decimal number with maximum 2 decimal places.")]
+        public string price { get; set; }
+
+        //[Required(ErrorMessage = "Quantity Available is required."), Display(Name = "Quantity Available")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number"), Display(Name = "Quantity Available")]
+        public int quantity_available { get; set; }
+    }
+}
